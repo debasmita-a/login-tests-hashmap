65ea36bd62df3ee66dca177bc9017aad10ba7cd3
Debasmita Adhikari <72942875+debasmita-a@users.noreply.github.com>
2024-06-25 16:59:09 +0530

    data provider with Iterator[]

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,7 +1,11 @@
 package pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
 
@@ -16,17 +20,41 @@ public class LoginPage {
 	private By submit = By.id("submit");
 	
 	private By loginSuccessMsg = By.xpath("//h1");
-	private By errorMsgUsername = By.id("error");
+	private By errorMsg = By.id("error");
 	
 	private By logout = By.linkText("Log out");
 	
-	public String doLogin(String un, String pass) {
+	public void doLogin(String un, String pass) {
 		driver.findElement(username).sendKeys(un);
 		driver.findElement(password).sendKeys(pass);
-		driver.findElement(submit).click();		
+		driver.findElement(submit).click();			
+	}
+	
+	public String doLoginWithCorrectData(String un, String pass) {
+		doLogin(un, pass);
 		String msg = driver.findElement(loginSuccessMsg).getText();
 		driver.findElement(logout).click();
 		return msg;
+	}
+	
+	public String doLoginWithInCorrectData(String un, String pass) {
+		doLogin(un, pass);
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		String msg = wait.until(ExpectedConditions.visibilityOfElementLocated(errorMsg)).getText();
+		return msg;
+	}
+	
+	public String doGenericLogin(String un, String pass) {
+		doLogin(un, pass);
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		if(driver.getCurrentUrl().contains("logged-in-successfully")) {
+			String msg = wait.until(ExpectedConditions.visibilityOfElementLocated(loginSuccessMsg)).getText();
+			driver.findElement(logout).click();
+			return msg;
+		}else{
+			String msg = wait.until(ExpectedConditions.visibilityOfElementLocated(errorMsg)).getText();
+			return msg;
+		}
 	}
 	
 
--- src/test/java/tests/LoginPageTest.java
+++ src/test/java/tests/LoginPageTest.java
@@ -1,49 +1,93 @@
 package tests;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import basetest.BaseTest;
 
-public class LoginPageTest extends BaseTest{
+public class LoginPageTest extends BaseTest {
 
-	@DataProvider
-	public Object[][] loginTestData(){
+	@DataProvider(name = "hashmap_dp")
+	public static Object[][] loginTestData() {
 		List<Map<String, String>> listOfMaps = new ArrayList<Map<String, String>>();
 		Map<String, String> testdataMap1 = new HashMap<>();
 		testdataMap1.put("username", "student");
 		testdataMap1.put("password", "Password123");
-		testdataMap1.put("successMsg", "Logged In Successfully");
+		testdataMap1.put("msg", "Logged In Successfully");
 		Map<String, String> testdataMap2 = new HashMap<>();
 		testdataMap2.put("username", "incorrectUser");
 		testdataMap2.put("password", "Password123");
-		testdataMap2.put("errorMsg", "Your username is invalid!");
+		testdataMap2.put("msg", "Your username is invalid!");
 		Map<String, String> testdataMap3 = new HashMap<>();
 		testdataMap3.put("username", "student");
 		testdataMap3.put("password", "incorrectPassword");
-		testdataMap3.put("errorMsg", "Your password is invalid!");
+		testdataMap3.put("msg", "Your password is invalid!");
 		listOfMaps.add(testdataMap1);
 		listOfMaps.add(testdataMap2);
 		listOfMaps.add(testdataMap3);
-		Object[][] testData = new Object[1][1];
-		testData[0][0] = listOfMaps;
-		return testData;
+
+		return new Object[][] { { listOfMaps } };
 	}
-	
-	@Test(dataProvider = "loginTestData")
+
+	@Test(dataProvider = "hashmap_dp")
 	public void doLoginTest(List<Map<String, String>> data) {
-		System.out.println(data);	
-		for(int i=0; i<data.size(); i++) {
-			String msg = loginPage.doLogin(data.get(i).get("username"), data.get(i).get("password"));
-			System.out.println(msg);
+		// System.out.println(data);
+		for (int i = 0; i < data.size(); i++) {
+			String msg = loginPage.doLoginWithInCorrectData(data.get(i).get("username"), data.get(i).get("password"));
+			Assert.assertEquals(msg, data.get(i).get("msg"));
 		}
-		
-		
+
 	}
-	
+
+	@Test(dataProvider = "hashmap_dp")
+	public void doGenericLoginTest1(List<Map<String, String>> data) {
+		for (int i = 0; i < data.size(); i++) {
+			String msg = loginPage.doGenericLogin(data.get(i).get("username"), data.get(i).get("password"));
+			System.out.println(msg);
+			Assert.assertEquals(msg, data.get(i).get("msg"));
+		}
+	}
+
+	@DataProvider(name = "iterator_dp")
+	public Iterator<Object[]> loginTestData2() {
+		Collection<Object[]> dataCollection = new ArrayList<Object[]>();
+		List<Map<String, String>> listOfMaps = new ArrayList<Map<String, String>>();
+		Map<String, String> testdataMap1 = new HashMap<>();
+		testdataMap1.put("username", "student");
+		testdataMap1.put("password", "Password123");
+		testdataMap1.put("msg", "Logged In Successfully");
+		Map<String, String> testdataMap2 = new HashMap<>();
+		testdataMap2.put("username", "incorrectUser");
+		testdataMap2.put("password", "Password123");
+		testdataMap2.put("msg", "Your username is invalid!");
+		Map<String, String> testdataMap3 = new HashMap<>();
+		testdataMap3.put("username", "student");
+		testdataMap3.put("password", "incorrectPassword");
+		testdataMap3.put("msg", "Your password is invalid!");
+		listOfMaps.add(testdataMap1);
+		listOfMaps.add(testdataMap2);
+		listOfMaps.add(testdataMap3);
+		for (Map<String, String> maps : listOfMaps) {			
+			dataCollection.add(maps.values().toArray(new Object[0]));	
+		}
+		return dataCollection.iterator();
+	}
+
+	@Test(dataProvider = "iterator_dp")
+	public void doGenericLoginTest(Map<String, String> map) {
+		String msg = loginPage.doGenericLogin(map.get("username"), map.get("password"));
+		System.out.println(msg);
+		Assert.assertEquals(msg, map.get("msg"));
+	}
+
 }
